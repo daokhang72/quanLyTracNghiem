d8961bb4e46963501127c9ec000f0faff0582418
daokhang72 <151323420+daokhang72@users.noreply.github.com>
2025-03-06 11:13:47 +0700

    chỉnh sửa ThongTinCaNhanPanel

--- QLTN/src/Component/ThongTinCaNhanPanel.java
+++ QLTN/src/Component/ThongTinCaNhanPanel.java
@@ -58,7 +58,6 @@ public class ThongTinCaNhanPanel extends JPanel {
         JLabel userLabel = new JLabel("User Name");
         userLabel.setFont(labelFont);
         mainPanel.add(userLabel, gbc);
-
         gbc.gridx = 2;
         JTextField nameValueLabel = new JTextField(userDto.getUserName());
         nameValueLabel.setFont(fieldFont);
@@ -73,33 +72,58 @@ public class ThongTinCaNhanPanel extends JPanel {
         JLabel emailLabel = new JLabel("Full Name");
         emailLabel.setFont(labelFont);
         mainPanel.add(emailLabel, gbc);
-        
         gbc.gridx = 2;
-        JTextField emailField = new JTextField(userDto.getUserFullName());
-        emailField.setFont(fieldFont);
-        mainPanel.add(emailField, gbc);
+        JTextField fullNameField = new JTextField(userDto.getUserFullName());
+        fullNameField.setFont(fieldFont);
+        mainPanel.add(fullNameField, gbc);
 
         gbc.gridx = 1;
         gbc.gridy = 2;
         JLabel usernameLabel = new JLabel("Email Address");
         usernameLabel.setFont(labelFont);
         mainPanel.add(usernameLabel, gbc);
-        
         gbc.gridx = 2;
-        JTextField userField = new JTextField(userDto.getUserEmail());
-        userField.setFont(fieldFont);
-        mainPanel.add(userField, gbc);
+        JTextField emailField = new JTextField(userDto.getUserEmail());
+        emailField.setFont(fieldFont);
+        mainPanel.add(emailField, gbc);
        
         JPanel btnPanel = new JPanel();
         btnPanel.setBackground(Color.WHITE);
         FadeButton btnSave = new FadeButton(new Color(230, 247, 230),new Color(0, 150, 136),new Color(0, 0, 0),"Cập Nhập Thông Tin");
         btnPanel.add(btnSave);
-//        btnSave.addActionListener(new ActionListener() {
-//			@Override
-//			public void actionPerformed(ActionEvent e) {
-//				String 
-//			}
-//		});
+        btnSave.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String username = nameValueLabel.getText().trim();
+				String email = emailField.getText().trim();
+				String fullname = fullNameField.getText().trim();
+				if (username.isEmpty() || email.isEmpty() || fullname.isEmpty()) {
+			        JOptionPane.showMessageDialog(ThongTinCaNhanPanel.this, "Vui lòng điền đầy đủ thông tin!", "Lỗi", JOptionPane.ERROR_MESSAGE);
+			        return;
+			    }
+				if (!email.matches("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$")) {
+		            JOptionPane.showMessageDialog(ThongTinCaNhanPanel.this, "Email không hợp lệ!", "Lỗi", JOptionPane.ERROR_MESSAGE);
+		            return;
+		        }
+				if (!username.equals(userDto.getUserName()) && userBll.kiemTraTonTai(username)) {
+		            JOptionPane.showMessageDialog(ThongTinCaNhanPanel.this, "Tên tài khoản đã tồn tại", "Lỗi", JOptionPane.ERROR_MESSAGE);
+		            return;
+		        }
+				if (username.equals(userDto.getUserName()) && fullname.equals(userDto.getUserFullName()) && email.equals(userDto.getUserEmail())) {
+			            JOptionPane.showMessageDialog(ThongTinCaNhanPanel.this, "Bạn chưa thay đổi thông tin nào!", "Thông báo", JOptionPane.INFORMATION_MESSAGE);
+			            return;
+			        }
+				userDto.setUserName(username);
+				userDto.setUserFullName(fullname);
+				userDto.setUserEmail(email);
+				boolean isupdated = userBll.updateUser(userDto);
+				if (isupdated) {
+			        JOptionPane.showMessageDialog(ThongTinCaNhanPanel.this, "Cập nhật thông tin thành công!", "Thành công", JOptionPane.INFORMATION_MESSAGE);
+			    } else {
+			        JOptionPane.showMessageDialog(ThongTinCaNhanPanel.this, "Cập nhật thất bại!", "Lỗi", JOptionPane.ERROR_MESSAGE);
+			    }
+			}
+		});
 
         add(mainPanel, BorderLayout.CENTER);
         add(btnPanel, BorderLayout.SOUTH);
